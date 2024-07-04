79b3d263c06b29b4471dd60f8538edbe7a87c582
A B <ab@As-Mac-mini.fritz.box>
2024-07-04 21:22:19 +0200

    Halbe Sterne ergänzt

--- zeugnis/templatetags/custom_tags.py
+++ zeugnis/templatetags/custom_tags.py
@@ -2,12 +2,13 @@ from django import template
 import math
 
 register = template.Library()
-
+'''
 @register.filter(name='custom_range')
 def custom_range(value):
     if value is None:
         return []
     return range(1, int(value) + 1)
+
 
 @register.filter(name='to_stars')
 def to_stars(avg_grade):
@@ -15,13 +16,37 @@ def to_stars(avg_grade):
         return 0
     if avg_grade <= 1.0:
         return 5
+    elif avg_grade <= 1.5:
+        return 4.5
     elif avg_grade <= 2.0:
         return 4
+    elif avg_grade <= 2.5:
+        return 3.5
     elif avg_grade <= 3.0:
         return 3
+    elif avg_grade <= 3.5:
+        return 2.5
     elif avg_grade <= 4.0:
         return 2
+    elif avg_grade <= 4.5:
+        return 1.5
     else:
         return 1
-    
+'''
+@register.filter(name='custom_range')
+def custom_range(value, arg=None):
+    if arg is None:
+        start, end = 1, value
+    else:
+        start, end = value, arg
+    return range(start, end + 1)
 
+@register.filter(name='to_stars')
+def to_stars(avg_grade):
+    if avg_grade is None:
+        return 0
+    return avg_grade
+
+@register.filter(name='add')
+def add(value, arg):
+    return value + arg
